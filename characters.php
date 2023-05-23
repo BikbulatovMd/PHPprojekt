5b8d2d6ef2bbe21eca700d368d5fa497a0e8ae40
BikbulatovMD <bikbulatov.stepan@gmail.com>
2023-05-23 12:27:30 +0300

    php

---
+++ characters.php
@@ -0,0 +1,245 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65"
+      crossorigin="anonymous"
+    />
+    <link rel="stylesheet" href="style/main.css" />
+    <link href="https://fonts.googleapis.com/css?family=Akaya Kanadaka" rel="stylesheet" />
+    <title>Characters</title>
+  </head>
+  <body>
+    <header class="header">
+      <nav class="navbar navbar-expand-lg bg-light">
+        <div class="container">
+          <a class="navbar-brand" href="./index.html">
+            LOTR
+            <img src="./img/logo2.png" alt="Logo" width="30" height="24" class="d-inline-block align-text-top ms-2" />
+          </a>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="offcanvas"
+            data-bs-target="#offcanvasNavbar"
+            aria-controls="offcanvasNavbar"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div
+            class="offcanvas offcanvas-end"
+            tabindex="-1"
+            id="offcanvasNavbar"
+            aria-labelledby="offcanvasNavbarLabel"
+          >
+            <div class="offcanvas-header">
+              <h5 class="offcanvas-title" id="offcanvasNavbarLabel">LOTR</h5>
+              <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+            </div>
+            <div class="offcanvas-body">
+              <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+                <li class="nav-item">
+                  <a class="nav-link" aria-current="page" href="/">Home</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="/movies.html">Movies</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="/gallery.html">Gallery</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="/games.html">Games</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="/quiz.html">Quiz</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link active" href="/characters.html">Character</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="/contacts.html">Contacts</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </header>
+    <main>
+      <section class="container">
+        <h2>Character Knowledge Base</h2>
+        <div class="card-list" style="color: black">
+          <div class="myCardWrapper">
+            <div class="card myCard">
+              <img src="./img/aragorn.jpg" class="card-img-top myCardImg" alt="Aragorn" />
+              <div class="card-body myCardBody">
+                <h5 class="card-title myCardTitle">Aragorn</h5>
+                <p class="card-text myCardText">
+                  The last leader of the pathfinders and the first king of the Reunited Kingdom, a direct descendant of
+                  the northern branch of the royal family of Elendil (along the line of Isildur, its elder branch — the
+                  dynasty of Amlait of Artedain). One of the last representatives of the Numenorians.
+                </p>
+              </div>
+              <div class="card-bottom">
+                <a href="./index.html" class="card-link">Read more...</a>
+              </div>
+            </div>
+          </div>
+          <div class="myCardWrapper">
+            <div class="card myCard">
+              <img src="./img/marry.jpg" class="card-img-top myCardImg" alt="Marry" />
+              <div class="card-body myCardBody">
+                <h5 class="card-title myCardTitle">Marry</h5>
+                <p class="card-text myCardText">
+                  Meriadoc Brandybuck, usually called Merry, is a Hobbit, a fictional character from J. R. R. Tolkien's
+                  Middle-earth legendarium, featured throughout his most famous work, The Lord of the Rings.
+                </p>
+              </div>
+              <div class="card-bottom">
+                <a href="./index.html" class="card-link">Read more...</a>
+              </div>
+            </div>
+          </div>
+          <div class="myCardWrapper">
+            <div class="card myCard">
+              <img src="./img/sam.jpg" class="card-img-top myCardImg" alt="Sam" />
+              <div class="card-body myCardBody">
+                <h5 class="card-title myCardTitle">Sam</h5>
+                <p class="card-text myCardText">
+                  Samwise Gamgee, known as Sam, was a hobbit of the Shire. He was Frodo Baggins' gardener and best
+                  friend. Sam proved himself to be Frodo's closest and most dependable companion, the most loyal of the
+                  Fellowship of the Ring, and played a critical role in protecting Frodo and destroying the One Ring.
+                </p>
+              </div>
+              <div class="card-bottom">
+                <a href="./index.html" class="card-link">Read more...</a>
+              </div>
+            </div>
+          </div>
+          <div class="myCardWrapper">
+            <div class="card myCard">
+              <img src="./img/sauron.jpg" class="card-img-top myCardImg" alt="Sauron" />
+              <div class="card-body myCardBody">
+                <h5 class="card-title myCardTitle">Sauron</h5>
+                <p class="card-text myCardText">
+                  Sauron, the eponymous Lord of the Rings, was the greatest subversive Maia, the maker of the One Ring,
+                  renowned student of the Vala Aulë, and the most trusted lieutenant of Morgoth. After Morgoth's ousting
+                  by the Valar,
+                </p>
+              </div>
+              <div class="card-bottom">
+                <a href="./index.html" class="card-link">Read more...</a>
+              </div>
+            </div>
+          </div>
+          <div class="myCardWrapper">
+            <div class="card myCard">
+              <img src="./img/gendalf.jpg" class="card-img-top myCardImg" alt="Gendalf" />
+              <div class="card-body myCardBody">
+                <h5 class="card-title myCardTitle">Gandalf</h5>
+                <p class="card-text myCardText">
+                  Gandalf the Grey, later known as Gandalf the White, and originally named Olórin, was an Istar
+                  (Wizard), dispatched to Middle-earth in the Third Age to combat the threat of Sauron. He joined Thorin
+                  II Oakenshield and his company to reclaim the Lonely Mountain from Smaug, helped form the Fellowship
+                  of the Ring to destroy the One Ring, and led the Free Peoples in the final campaign of the War of the
+                  Ring.
+                </p>
+              </div>
+
+              <div class="card-bottom">
+                <a href="./index.html" class="card-link">Read more...</a>
+              </div>
+            </div>
+          </div>
+          <div class="myCardWrapper">
+            <div class="card myCard">
+              <img src="./img/bilbo.jpg" class="card-img-top myCardImg" alt="Bilbo" />
+              <div class="card-body myCardBody">
+                <h5 class="card-title myCardTitle">Bilbo</h5>
+                <p class="card-text myCardText">
+                  Bilbo Baggins was a Hobbit of the Shire, the main protagonist of The Hobbit and a secondary character
+                  in The Lord of the Rings. Gandalf suggested to Thorin and Company that they hire Bilbo Baggins to be
+                  their burglar in the Quest of Erebor, and later fought in the Battle of Five Armies
+                </p>
+              </div>
+
+              <div class="card-bottom">
+                <a href="./index.html" class="card-link">Read more...</a>
+              </div>
+            </div>
+          </div>
+          <div class="myCardWrapper">
+            <div class="card myCard">
+              <img src="./img/frodo.jpg" class="card-img-top myCardImg" alt="Frodo" />
+              <div class="card-body myCardBody">
+                <h5 class="card-title myCardTitle">Frodo</h5>
+                <p class="card-text myCardText">
+                  Frodo Baggins, son of Drogo Baggins, was a hobbit of the Shire in the late Third Age. He is commonly
+                  considered Tolkien's most renowned character for his leading role in the Quest of the Ring, in which
+                  he bore the One Ring to Mount Doom, where it was destroyed. He was a Ring-bearer, best friend to his
+                  gardener, Samwise Gamgee, and was one of three hobbits who sailed from Middle-earth to the Uttermost
+                  West at the end of the Third Age.
+                </p>
+              </div>
+
+              <div class="card-bottom">
+                <a href="./index.html" class="card-link">Read more...</a>
+              </div>
+            </div>
+          </div>
+          <div class="myCardWrapper">
+            <div class="card myCard">
+              <img src="./img/legolas.jpg" class="card-img-top myCardImg" alt="Legolas" />
+              <div class="card-body myCardBody">
+                <h5 class="card-title myCardTitle">Legolas</h5>
+                <p class="card-text myCardText">
+                  Legolas was a Sindar Elf who was part of the Fellowship of the Ring in the Third Age. Son of the
+                  Elvenking Thranduil of Mirkwood, Legolas was Mirkwood's prince, a messenger, and a master archer. With
+                  his keen eyesight, sensitive hearing, and excellent bowmanship, Legolas was valuable to the Fellowship
+                  in their journey across Middle-earth. He was well-known for becoming friends with the Dwarf Gimli,
+                  despite their long-held differences.
+                </p>
+              </div>
+
+              <div class="card-bottom">
+                <a href="./index.html" class="card-link">Read more...</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </main>
+
+    <footer class="footer">
+      <div class="container">
+        <ul class="socials">
+          <li class="socials-item">
+            <a href="tel:+421950788221" class="socials-link">
+              <img src="./img/icons/whatsapp.svg" alt="tel number" />+421950234987
+            </a>
+          </li>
+          <li class="socials-item">
+            <a href="https://edu.ukf.sk/user/profile.php?id=29446" target="_blank" class="socials-link">
+              <img src="./img/icons/instagram.svg" alt="instagram" />@stepahkaaa
+            </a>
+          </li>
+          <li class="socials-item author">
+            <a href="mailto:" target="_blank" class="socials-link"> bikbulatov.stepan@gmail.com </a>
+          </li>
+        </ul>
+        <div class="copyright">&#169;All rights reserved.</div>
+      </div>
+    </footer>
+    <script src="/main.js"></script>
+    <script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4"
+      crossorigin="anonymous"
+    ></script>
+  </body>
+</html>
