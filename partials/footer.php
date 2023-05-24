61b11a0012b4886e847cd9002c40dd01332f8ae7
BikbulatovMD <bikbulatov.stepan@gmail.com>
2023-05-24 17:01:01 +0300

    update

--- partials/footer.php
+++ partials/footer.php
@@ -18,3 +18,9 @@
       <div class="copyright">&#169;All rights reserved.</div>
     </div>
   </footer>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+  <script src="js/main.js"></script>
+  </body>
+
+  </html>
--- partials/header.php
+++ partials/header.php
@@ -5,14 +5,13 @@ include('inc/config.php');
 <html lang="sk">
 
 <head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Stránka</title>
-  <link rel="stylesheet" href="css/accordion.css">
-  <link rel="stylesheet" href="css/form.css">
-  <link rel="stylesheet" href="css/style.css">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous" />
+  <link rel="stylesheet" href="style/main.css" />
+  <link href="https://fonts.googleapis.com/css?family=Akaya Kanadaka" rel="stylesheet" />
+  <title>Document</title>
 
 </head>
 
