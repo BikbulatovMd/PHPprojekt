06eb85b05c2468a588aff749482eafecfd2e5c68
BikbulatovMD <bikbulatov.stepan@gmail.com>
2023-05-24 21:37:28 +0300

    admin

--- inc/contact/select.php
+++ inc/contact/select.php
@@ -1,7 +1,19 @@
 <?php
 $contact = $Contact->get_contact();
+echo '<section class="container">';
 echo '<table class="admin-table">';
+echo '<div class="table-wrap">';
+echo '<table class="table table-bordered table-light table-striped">';
+echo '<thead>';
+echo '<tr>';
+echo '  <th>Name</th>';
+echo ' <th>E-mail</th>';
+echo '  <th>Comment</th>';
+echo '  <th>Delete</th>';
+echo ' </tr>';
+echo '</thead>';
 foreach ($contact as $c) {
+  echo '<tbody class="table-group-divider">';
   echo '<tr>';
   echo '<td>' . $c->contact_name;
   '</td>';
@@ -10,13 +22,11 @@ foreach ($contact as $c) {
   echo '<td>' . $c->comment;
   '</td>';
   echo '<td>
-                            <form action="inc/contact/edit.php" method="post">
-                                <button type = "submit" name="edit_contact" value="' . $c->id . '"' . '>Editovať</button>
-                            </form></td>';
-  echo '<td>
                             <form action="inc/contact/delete.php" method="post">
-                                <button type = "submit" name="delete_contact" value="' . $c->id . '"' . '>Vymazať</button>
+                                <button type = "submit" name="delete_contact" value="' . $c->id . '"' . '>Delete</button>
                             </form></td>';
   echo '</tr>';
+  echo '</div>';
 }
 echo '</table>';
+echo '</section>';
