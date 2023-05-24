a541fc3754008527eb70294fb573ae07b8fea331
BikbulatovMD <bikbulatov.stepan@gmail.com>
2023-05-24 17:03:50 +0300

    update

--- index.php
+++ index.php
@@ -1,39 +1,26 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
+include('partials/header.php');
+?>
 
-<head>
-  <meta charset="UTF-8" />
-  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous" />
-  <link href="https://fonts.googleapis.com/css?family=Akaya Kanadaka" rel="stylesheet" />
-  <title>Document</title>
-</head>
-
-<body>
-  <?php
-  include('partials/header.php');
-  ?>
-
-  <main class="container">
-    <div class="page-wrapper scrollspy">
+<main class="container">
+  <div class="page-wrapper scrollspy">
+    <?php
+    include('partials/scroll.php');
+    ?>
+    <div data-bs-spy="scroll" data-bs-target="#list-example" data-bs-smooth-scroll="true" class="scrollspy-example" tabindex="0">
       <?php
-      include('partials/scroll.php');
+      include('partials/accordion.php');
       ?>
-      <div data-bs-spy="scroll" data-bs-target="#list-example" data-bs-smooth-scroll="true" class="scrollspy-example" tabindex="0">
-        <?php
-        include('partials/accordion.php');
-        ?>
-      </div>
     </div>
+  </div>
 
-    <div class="go-top hide d-flex d-sm-none">
-      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-up" viewBox="0 0 16 16">
-        <path fill-rule="evenodd" d="M8 15a.5.5 0 0 0 .5-.5V2.707l3.146 3.147a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L7.5 2.707V14.5a.5.5 0 0 0 .5.5z" />
-      </svg>
-    </div>
-  </main>
+  <div class="go-top hide d-flex d-sm-none">
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-up" viewBox="0 0 16 16">
+      <path fill-rule="evenodd" d="M8 15a.5.5 0 0 0 .5-.5V2.707l3.146 3.147a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L7.5 2.707V14.5a.5.5 0 0 0 .5.5z" />
+    </svg>
+  </div>
+</main>
 
-  <?php
-  include('partials/footer.php');
-  ?>
+<?php
+include('partials/footer.php');
+?>
--- quiz.php
+++ quiz.php
@@ -1,48 +1,34 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
+include('partials/header.php');
+?>
 
-<head>
-  <meta charset="UTF-8" />
-  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous" />
-  <link href="https://fonts.googleapis.com/css?family=Akaya Kanadaka" rel="stylesheet" />
-  <link rel="stylesheet" href="style/main.css" />
-  <title>Document</title>
-</head>
-
-<body>
-  <?php
-  include('partials/header.php');
-  ?>
-
-  <main class="container d-flex align-items-center justify-content-center">
-    <div class="quiz-box" id="quiz">
-      <div class="quiz-count">0/0</div>
-      <div class="quiz-header">
-        <h2 id="question">Question text</h2>
-        <ul class="answer-list">
-          <li class="answer-wrapper">
-            <input type="radio" name="answer" id="a" class="answer" />
-            <label for="a" id="a_text">Answer 1</label>
-          </li>
-          <li class="answer-wrapper">
-            <input type="radio" name="answer" id="b" class="answer" />
-            <label for="b" id="b_text">Answer 2</label>
-          </li>
-          <li class="answer-wrapper">
-            <input type="radio" name="answer" id="c" class="answer" />
-            <label for="c" id="c_text">Answer 3</label>
-          </li>
-        </ul>
-      </div>
-      <div class="quiz-buttons">
-        <button id="restart" type="button" class="btn btn-secondary btn-lg me-2">Restart</button>
-        <button id="submit" type="button" class="btn btn-secondary btn-lg">Next</button>
-      </div>
+<main class="container d-flex align-items-center justify-content-center">
+  <div class="quiz-box" id="quiz">
+    <div class="quiz-count">0/0</div>
+    <div class="quiz-header">
+      <h2 id="question">Question text</h2>
+      <ul class="answer-list">
+        <li class="answer-wrapper">
+          <input type="radio" name="answer" id="a" class="answer" />
+          <label for="a" id="a_text">Answer 1</label>
+        </li>
+        <li class="answer-wrapper">
+          <input type="radio" name="answer" id="b" class="answer" />
+          <label for="b" id="b_text">Answer 2</label>
+        </li>
+        <li class="answer-wrapper">
+          <input type="radio" name="answer" id="c" class="answer" />
+          <label for="c" id="c_text">Answer 3</label>
+        </li>
+      </ul>
     </div>
-  </main>
+    <div class="quiz-buttons">
+      <button id="restart" type="button" class="btn btn-secondary btn-lg me-2">Restart</button>
+      <button id="submit" type="button" class="btn btn-secondary btn-lg">Next</button>
+    </div>
+  </div>
+</main>
 
-  <?php
-  include('partials/footer.php');
-  ?>
+<?php
+include('partials/footer.php');
+?>
